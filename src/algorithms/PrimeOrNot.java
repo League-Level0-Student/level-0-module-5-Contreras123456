e5bc1fae8c7b124d76237bc1ed30b011d9934079
kzhuo123 <League@administorsimac.attlocal.net>
2018-11-03 12:51:52 -0700

    Did more hard stuff in coding.

--- src/algorithms/PrimeOrNot.java
+++ src/algorithms/PrimeOrNot.java
@@ -1,10 +1,23 @@
 package algorithms;
 
-public class PrimeOrNot {
+import javax.swing.JOptionPane;
 
+public class PrimeOrNot {
+	
 	public static void main(String[] args) {
-	for (int i = 1; i < 101; i++) {
-		
+		 String taco = JOptionPane.showInputDialog("Guess a number that is prime.");
+	int a=Integer.parseInt(taco);
+		int count=0;
+	for (int i = 1; i <= a; i++) {
+		if (a%i==0) {
+			count++;
+		}
+	}
+	if(count>2) {
+		JOptionPane.showMessageDialog(null, "The number is not prime.");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "The number is prime.");
 	}
 	}
 }
