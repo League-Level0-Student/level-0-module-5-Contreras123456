7f14b796bcd0a3a1d7d758f308afe2bda167956d
GT <League@administorsimac.attlocal.net>
2018-11-10 12:54:44 -0800

    Did more work on ellipse.

--- src/algorithms/Fibonacci.java
+++ src/algorithms/Fibonacci.java
@@ -2,4 +2,18 @@ package algorithms;
 
 public class Fibonacci {
 
+public static void main(String[] args) {
+	int newnumber = 0;
+	int first = 0;
+	int second = 1;
+	System.out.println(first);
+	System.out.println(second);
+	for (int i = 0; i < 11 ; i++) {
+		 newnumber = first+second;
+		System.out.println(newnumber);
+		first = second;
+		second = newnumber;
+		
+	}
+}
 }
