edbb95166f6590b9d0924c8a3fd1cbbfab33223d
1luckybiscuit <lukasnepo11@gmail.com>
2018-11-17 12:53:10 -0800

    Did more coding at a workship

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -11,34 +11,43 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-		String Roblox = JOptionPane.showInputDialog("What is your name?");
+		goofyName = JOptionPane.showInputDialog("What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
 		// Run your program to see that this works.
-		System.out.println(Roblox.toUpperCase());
+		System.out.println(goofyName.toUpperCase());
 
 		// 3. Loop through each character of the name (steps 4 - 6).
 		// HINT: Use .length() to determine the number of characters in the String.
-		System.out.println(Roblox.length());
+		System.out.println(goofyName.length());
 		// 4. Create a String variable to store the next character of the name
 		// using .substring(start, end)
 		// HINT: replace 'start' and 'end' to get String with character at i
-		for (int i = 1; i <= 5; i++) {
+		for (int i = 1; i < goofyName.length(); i++) {
 
-			String oof = Roblox.substring(0, i);
+			String oof = goofyName.substring(0, i);
 			System.out.println(oof);
-		}
-
-		for (int i = 1; i < 5; i++) {
-			
-		}
 		
-		// 5. Use MODULO to set this variable to upper case for EVEN characters
+
+		
+		
+		
+// 5. Use MODULO to set this variable to upper case for EVEN characters
 		// and lower case for ODD characters.
+if (i%2==0) {
+	oof.toUpperCase();
+}
+else {
+	goofyName.toLowerCase();
+}
 
 		// 6. ADD this String (containing 1 char) to the goofyName String
-
+String s1 = "oof";
+String s2 = "goofyName";
+ goofyName.concat(oof);
+		}
 		// 7. Use pop-up to show user their Goofy name
-
+		System.out.println(goofyName);
+JOptionPane.showMessageDialog(null, "Here is your Goofy name."+ goofyName);
 	}
 
 	private static void toUpperCase() {
