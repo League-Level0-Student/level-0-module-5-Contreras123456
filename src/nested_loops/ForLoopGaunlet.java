f88f4ed7c83bb2053395bd02848a2fa116820582
league <league@wts5.attlocal.net>
2018-12-01 12:52:06 -0800

    More coding on an assignment.

--- src/nested_loops/ForLoopGaunlet.java
+++ src/nested_loops/ForLoopGaunlet.java
@@ -1,11 +1,23 @@
 package nested_loops;
 
-public class ForLoopGaunlet {
 
-	public static void main(String[] args) {
-		for (int i = 0; i < ; i++) {
+public class ForLoopGaunlet {
+public static void main(String[] args) {
+	
+		for (int i = 0; i < 101  ; i++) {
+			System.out.println(i);
+		}
+		for (int j = 100; j > 0 ; j--) {
+		System.out.println(j);	
+		}
+		for (int e = 2; e < 101 ; e++) {
+			if (e%2==0) {
+				System.out.println(e);
+			}
+				for (int i = 0; i < args.length; i++) {
+					
+				}
 			
 		}
-		
 	}
 }
