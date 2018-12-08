cc0f64fe955ea12248357787383018b99abf7a65
league <league@wts5.attlocal.net>
2018-12-08 12:55:45 -0800

    Did more work at coding.

--- src/nested_loops/ForLoopGaunlet.java
+++ src/nested_loops/ForLoopGaunlet.java
@@ -7,17 +7,56 @@ public static void main(String[] args) {
 		for (int i = 0; i < 101  ; i++) {
 			System.out.println(i);
 		}
-		for (int j = 100; j > 0 ; j--) {
-		System.out.println(j);	
+		for (int i = 100; i > 0 ; i--) {
+		System.out.println(i);	
 		}
-		for (int e = 2; e < 101 ; e++) {
-			if (e%2==0) {
-				System.out.println(e);
+		for (int i = 2; i < 101 ; i++) {
+			if (i%2==0) {
+				System.out.println(i);
 			}
-				for (int i = 0; i < args.length; i++) {
+				for (int j = 1; j < 99; j+=2) {
+						System.out.println(j);
 					
 				}
-			
+			for (int j = 0; j < 501; j++) {
+				if (j%2==0) {
+					System.out.println(j+"is even");
+				}
+				else {
+					System.out.println(j+"is odd");
+				}
+				
+				
+			}
+			for (int n = 0; n <= 777; n+=7) {
+				System.out.println(n);
+			}
+			int year=2006;
+			for (int j = 0; j < 13; j++) {
+				System.out.println("In "+year+", I was "+ j +" years old");
+				year++;
+			}
 		}
-	}
-}
+		for (int i = 0; i <=2; i++) {
+			for (int j = 0; j <= 2; j++) {
+				System.out.println(i+" "+j);
+			}
+		}
+		
+		for (int i = 1; i <= 9; i++) {
+			for (int j = i; j <=i+2; j++) {
+				System.out.print(j+" ");
+		}
+				System.out.println();
+				i=i+2;
+			}
+			
+			
+			
+		
+		}
+				}
+			
+		
+	
+
