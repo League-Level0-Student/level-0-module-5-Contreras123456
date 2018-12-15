97cf5c91434c01a973602307e94a65cfa4a800d4
1luckybiscuit <lukasnepo11@gmail.com>
2018-12-15 12:50:49 -0800

    Did more coding at tloap

--- src/nested_loops/ForLoopGaunlet.java
+++ src/nested_loops/ForLoopGaunlet.java
@@ -43,20 +43,52 @@ public static void main(String[] args) {
 			}
 		}
 		
-		for (int i = 1; i <= 9; i++) {
-			for (int j = i; j <=i+2; j++) {
-				System.out.print(j+" ");
-		}
+		for (int i = 0; i < 3; i++) {
+			for (int j = 1; j <=3; j++) {
+				System.out.print((3*i+j)+" ");
+		    }
 				System.out.println();
-				i=i+2;
+		}
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.print(i+" ");
+		  
+		}
+
+		   
+		for (int i = 0; i < 10; i++) {
+			for (int j = 1; j <= 10; j++) {
+					
+			System.out.print((10*i+j)+" ");
+			  	    
 			}
-			
-			
-			
+			   System.out.println();
+		}
+		// Task 4
+		for (int i = 6; i >=1 ; i--) {
+			for (int j = i; j <=6 ; j++) {
+			    System.out.print("* ");
+			}
+			System.out.println();
 		
 		}
-				}
-			
 		
+		
+		}
+		}
+
+
+		
+		
+		
+				
+			
+			
+			
+	
+
+
+			
+	
 	
 
