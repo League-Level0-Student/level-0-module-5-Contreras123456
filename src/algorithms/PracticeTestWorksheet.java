8be473b56a1e015e2adbdd7fbc35d56560a6299a
league <league@wts5.attlocal.net>
2018-12-22 12:51:36 -0800

    More work at coding such as the practice test.

--- src/algorithms/PracticeTestWorksheet.java
+++ src/algorithms/PracticeTestWorksheet.java
@@ -1,5 +1,24 @@
 package algorithms;
 
-public class PracticeTestWorksheet {
+import javax.swing.JOptionPane;
 
+public class PracticeTestWorksheet {
+public static void main(String[] args) {
+	
+	String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+   int teletubbies = Integer.parseInt(dimes);
+   System.out.println("You have "+ teletubbies*10 + " cents");
+   int i = 36;
+   String up = JOptionPane.showInputDialog("How tall are you?");
+int height = Integer.parseInt(up);
+   if (height<i) {
+	JOptionPane.showMessageDialog(null, "You need to eat your wheaties." );
 }
+for (int j = 1; j < 30; j++) {
+	System.out.println(j*3);
+}
+}
+}
+
+
+
