deecf577c4c95b166ffddd2754ac8754bf6fdf19
Ddubby <league@wts-im-17.attlocal.net>
2018-12-29 12:55:35 -0800

    Did more work at coding.

--- src/algorithms/PracticeTestWorksheet.java
+++ src/algorithms/PracticeTestWorksheet.java
@@ -1,5 +1,7 @@
 package algorithms;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class PracticeTestWorksheet {
@@ -14,11 +16,31 @@ int height = Integer.parseInt(up);
    if (height<i) {
 	JOptionPane.showMessageDialog(null, "You need to eat your wheaties." );
 }
-for (int j = 1; j < 30; j++) {
+for (int j = 1; j < 11; j++) {
 	System.out.println(j*3);
 }
+
+Random youtuberewind = new Random();
+int 	defaultdance = youtuberewind.nextInt(20);
+System.out.println(defaultdance);
+
+Random roblox = new Random();
+int takethel = roblox.nextInt(10);
+System.out.println(takethel);
+JOptionPane.showMessageDialog(null, defaultdance-takethel);
+String orangejustice = JOptionPane.showInputDialog("What city do you live in?");
+if (orangejustice.equals("San Diego")) {
+	JOptionPane.showMessageDialog(null, "You live in America's Finest City.");
 }
+else {
+	JOptionPane.showMessageDialog(null, "Move to San Diego.");
 }
+int cars = 2;
+	JOptionPane.showMessageDialog(null,"There are 2 wheels the car has between them.");
+String nemo = JOptionPane.showInputDialog("What is the name of your school?");
+} 
+}
+
 
 
 
